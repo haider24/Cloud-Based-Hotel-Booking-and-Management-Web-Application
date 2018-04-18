30a9e159eafa649388a33a45d9d6b4fc1389d2cf
haider24 <haiderali9624@gmail.com>
2018-04-19 01:09:09 +0500

    test

--- djangotest/models.py
+++ djangotest/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Customers(models.Model):
+class Customer(models.Model):
     name=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     password=models.CharField(max_length=50)

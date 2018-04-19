d4c435d12db79eee3dc931e4c539881af08d9d3a
haider24 <haiderali9624@gmail.com>
2018-04-19 21:16:58 +0500

    test

--- djangotest/admin.py
+++ djangotest/admin.py
@@ -5,7 +5,7 @@ from djangotest.models import Customer
 # Register your models here.
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Email', 'Price')
+    list_display = ('name', 'email')
     list_display_links = None
     def has_add_permission(self, request):
         return False

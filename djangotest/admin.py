48e7cbde27ac865248c5198d72f48085ce30ac62
haider24 <haiderali9624@gmail.com>
2018-04-19 20:39:28 +0500

    test

--- djangotest/admin.py
+++ djangotest/admin.py
@@ -4,6 +4,13 @@ from djangotest.models import Customer
 
 # Register your models here.
 
-class PersonAdmin(admin.ModelAdmin):
-    readonly_fields = ('Customer',)
+class CustomerAdmin(admin.ModelAdmin):
+
+    def get_actions(self, request):
+        actions = super(CustomerAdmin, self).get_actions(request)
+        del actions['edit_permissions']
+        del actions['add_permissions']
+        return actions
+
+
 admin.site.register(Customer)

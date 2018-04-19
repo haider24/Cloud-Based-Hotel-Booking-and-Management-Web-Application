9937071e55aec9b5c4e830477dd3e553e6edc062
haider24 <haiderali9624@gmail.com>
2018-04-19 20:48:20 +0500

    test

--- djangotest/admin.py
+++ djangotest/admin.py
@@ -4,11 +4,11 @@ from djangotest.models import Customer
 
 # Register your models here.
 
-admin.site.register(Customer)
-
 class CustomerAdmin(admin.ModelAdmin):
     def get_actions(self, request):
         actions = super(CustomerAdmin, self).get_actions(request)
         del actions['edit_permissions']
         del actions['add_permissions']
         return actions
+
+admin.site.register(Customer,CustomerAdmin)

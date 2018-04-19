6cb9710fbb122619f7d87fde95fee3bbf8b4966d
haider24 <haiderali9624@gmail.com>
2018-04-19 21:04:51 +0500

    test

--- djangotest/admin.py
+++ djangotest/admin.py
@@ -9,7 +9,7 @@ class CustomerAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
+    def has_change_permission(self, request, obj=None):
         return False
     # def get_actions(self, request):
     #     actions = super(CustomerAdmin, self).get_actions(request)

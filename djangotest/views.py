bbc0cca9007c1da55fd8818d9285849eb7bb61ca
haider24 <haiderali9624@gmail.com>
2018-04-21 21:38:42 +0500

    test

--- djangotest/views.py
+++ djangotest/views.py
@@ -4,19 +4,19 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('homepage.html')
 
     # rendering the template in HttpResponse
-    context = {
-        'name': 'Belal Khan',
-        'fname': 'Azad Khan',
-        'course': 'Python Django Framework',
-        'address': 'Kanke, Ranchi, India',
-    }
+    # context = {
+    #     'name': 'Belal Khan',
+    #     'fname': 'Azad Khan',
+    #     'course': 'Python Django Framework',
+    #     'address': 'Kanke, Ranchi, India',
+    # }
 
     # rendering the template in HttpResponse
     # but this time passing the context and request
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render())
 # Create your views here.
 
 def loginpage(request):

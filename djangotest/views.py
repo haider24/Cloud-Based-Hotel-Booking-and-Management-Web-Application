398f02a9e468bdd3dc084e92c2de1f935e671927
haider24 <haiderali9624@gmail.com>
2018-04-29 20:46:44 +0500

    test

--- djangotest/views.py
+++ djangotest/views.py
@@ -9,16 +9,15 @@ from datetime import datetime, date
 
 
 def index(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated:     #if user is logged in then redirect to his profile page
         return redirect('profile')
     else:
         return render(request, 'homepage.html')
-# Create your views here.
 
 def signin(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated:    #if user is logged in then redirect to his profile page
         return redirect('profile')
-    if request.method == "POST":
+    if request.method == "POST":         #if signin request then authenticate user
         userName=request.POST.get('username')
         userPassword=request.POST.get('password')
         user=authenticate(username=userName,password=userPassword)
@@ -29,7 +28,7 @@ def signin(request):
             request.session['booking']=False
             request.session['userBookings']=getUserBookings(user)
             return HttpResponseRedirect('/profile')
-    else:
+    else:              #if get request send login page in response
         return render(request, 'login.html')
 
 def signout(request):
@@ -40,9 +39,9 @@ def signout(request):
 
 
 def signup(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated:      #if user is logged in then redirect to his profile page
         return redirect('profile')
-    if request.method == "POST":
+    if request.method == "POST":           #if post request then create new account
         name = request.POST.get('name')
         username = request.POST.get('username')
         email = request.POST.get('email')
@@ -52,32 +51,32 @@ def signup(request):
             checkUser = User.objects.get(username=username)
         except User.DoesNotExist:
             checkUser=None
-        if not checkUser is None:
+        if not checkUser is None:         #check if username is already taken
             return render(request,'signup.html',{'message':'Username Already taken'})
 
         try:
             checkUser = User.objects.get(email=email)
         except User.DoesNotExist:
             checkUser=None
-        if not checkUser is None:
+        if not checkUser is None:        #check if an account already exists with given email
             return render(request,'signup.html',{'message':'An account with this Email already exists'})
 
         user=User.objects.create_user(first_name=name,username=username,email=email,password=password)
         if image is not None:
             user.profile.profilePicture=image
-        user.save()
-        login(request,user)
+        user.save()                     #create account
+        login(request,user)             #start user session
         request.session['booking'] = False
         request.session['userBookings'] = getUserBookings(user)
-        return redirect('profile')
+        return redirect('profile')      #redirect to profile page
         #return HttpResponseRedirect('/profile')
     else:
-        return render(request, 'signup.html')
+        return render(request, 'signup.html')     #if get request then send signup page in response
 
 def profile(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:   #if user is not logged in then redirect to homepage
         return redirect('index')
-    if request.method=="POST":
+    if request.method=="POST":              #if post request then update user profile
         newName=request.POST.get('name')
         newEmail=request.POST.get('email')
         newImage=request.FILES.get('image')
@@ -87,14 +86,14 @@ def profile(request):
         if newImage is not None:
             user.profile.profilePicture = newImage
         user.save()
-        return render(request,'profile.html',{'message':'Profile updated Successfully'})
+        return render(request,'profile.html',{'message':'Profile updated Successfully'})    #send updated profile page with success message
     else:
-        return render(request, 'profile.html')
+        return render(request, 'profile.html')     #if get request then return user profile page in response
 
 def feedback(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:    #if user is not logged in then redirect to homepage
         return redirect('index')
-    if request.method == "POST":
+    if request.method == "POST":      #if post request then save comment
         customerComment=request.POST.get('comment')
         user=request.user
         comment=Comment(comment=customerComment,user=user)
@@ -102,35 +101,35 @@ def feedback(request):
         allComments=getComments()
         context={'comments':allComments}
         return render(request, 'feedback.html', context)
-    allComments=getComments
+    allComments=getComments           #if get request then send feedback page in response
     user=request.user
     context = {'comments': allComments}
     return render(request, 'feedback.html', context)
 
-def getComments():
+def getComments():     #helper function to get all comments
     comments=Comment.objects.all()
     return comments
 
 def rating(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:     #if user is not logged in then redirect to homepage
         return redirect('index')
-    if request.method == "POST":
+    if request.method == "POST":              #if post request then save rating
         customerRating=request.POST.get('rating')
         user=request.user
         user.profile.rating=customerRating
         user.save()
-        return HttpResponseRedirect('/profile')
+        return HttpResponseRedirect('/profile')   #redirect to profile page
 
 def changePassword(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:     #if user is not logged in then redirect to his homepage
         return redirect('index')
-    if request.method == "POST":
+    if request.method == "POST":      #if post request then change password
         user=request.user
         newPassword=request.POST.get('password')
         user.set_password(newPassword)
         user.save()
-        update_session_auth_hash(request, user)
-        return render(request, 'profile.html',{'message':'Password changed Successfully'})
+        update_session_auth_hash(request, user)     #update user session with new password
+        return render(request, 'profile.html',{'message':'Password changed Successfully'})   #return profile page with success message
 
 
 def test(request):
@@ -153,26 +152,26 @@ def test(request):
     return render(request,'test.html')
 
 def rooms(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:    #if user is not logged in then redirect to his homepage
         return redirect('index')
-    if request.method=="POST":
+    if request.method=="POST":          #if post request then check if selected room is available on given dates
         typeID=request.POST.get('id')
         checkinDate=request.POST.get('checkin')
         checkoutDate = request.POST.get('checkout')
-        room=findAvailableRooms(typeID,checkinDate,checkoutDate)
-        if room ==None:
+        room=findAvailableRooms(typeID,checkinDate,checkoutDate)   #find if room is available on given dates
+        if room ==None:             #room not available on given dates
             allRooms = RoomType.objects.all()
             allImages = Image.objects.all()
             context = {'rooms': allRooms, 'images': allImages,'message':'Room not available on selected dates, please try different room or select different dates'}
-            return render(request, 'rooms.html', context)
-        else:
+            return render(request, 'rooms.html', context)   #return rooms not available message
+        else:   #room available
             allImages = Image.objects.all()
             roomType=RoomType.objects.get(id=typeID)
             days=calculateDays(checkinDate,checkoutDate)
             bill=days*roomType.price
             request.session['booking'] = True
             context={'images':allImages,'checkin':checkinDate,'checkout':checkoutDate,'roomType':roomType,'roomid':room.id,'days':days,'bill':bill}
-            return render(request,'confirmbooking.html',context)
+            return render(request,'confirmbooking.html',context)    #return confirm booking page with booking details
     else:
         allRooms = RoomType.objects.all()
         allImages = Image.objects.all()
@@ -180,7 +179,7 @@ def rooms(request):
         return render(request, 'rooms.html', context)
 
 
-def findAvailableRooms(typeID,checkinDate,checkoutDate):
+def findAvailableRooms(typeID,checkinDate,checkoutDate):   #helper function to find available rooms
     allRooms=Room.objects.filter(type__id=typeID)
     if not allRooms:
         return None
@@ -189,14 +188,14 @@ def findAvailableRooms(typeID,checkinDate,checkoutDate):
             return room
     return None
 
-def checkifAvailable(room,checkinDate,checkoutDate):
+def checkifAvailable(room,checkinDate,checkoutDate):   #helper function to check of room is available on given dates
     roomBookings=Booking.objects.filter(room__id=room.id)
     if not roomBookings:
         return True
     else:
         return checkDates(roomBookings,checkinDate,checkoutDate)
 
-def checkDates(roomBookings,checkinDate,checkoutDate):
+def checkDates(roomBookings,checkinDate,checkoutDate):   #helper function
     year, month, day = checkinDate.split("-")
     convertedCheckin = date(int(year), int(month), int(day))
     year, month, day = checkoutDate.split("-")
@@ -209,7 +208,7 @@ def checkDates(roomBookings,checkinDate,checkoutDate):
 
     return True
 
-def calculateDays(checkin,checkout):
+def calculateDays(checkin,checkout):  #helper function to calulate days between checkin and checkout dates
     newCheckin = checkin.replace('-', '/')
     newCheckout = checkout.replace('-', '/')
     date_format = "%Y/%m/%d"
@@ -219,10 +218,10 @@ def calculateDays(checkin,checkout):
     return delta.days
 
 def booking(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:  #if user is not logged in then redirect to homepage
         return redirect('index')
     booinginProgress=request.session['booking']
-    if not booinginProgress:
+    if not booinginProgress:     #if booking is not in progress then redirect to profile page
         return redirect('index')
     roomID=request.POST.get('roomid')
     checkinDate=request.POST.get('checkin')
@@ -231,27 +230,27 @@ def booking(request):
     user=request.user
     days=calculateDays(checkinDate,checkoutDate)
     bill=room.type.price*days
-    booking=Booking.objects.create(user=user,room=room,checkin=checkinDate,checkout=checkoutDate,bill=bill)
+    booking=Booking.objects.create(user=user,room=room,checkin=checkinDate,checkout=checkoutDate,bill=bill) #add booking in database
     request.session['booking'] = False
     request.session['userBookings'] = getUserBookings(user)
     bookings = Booking.objects.filter(user__username=user.username)
     images = Image.objects.all()
-    return render(request, 'mybookings.html', {'bookings': bookings, 'images': images,'message':'Booking Successful!'})
+    return render(request, 'mybookings.html', {'bookings': bookings, 'images': images,'message':'Booking Successful!'}) #send booking successfull response
 
 def cancelBooking(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:   #if user is not logged in then redirect to homepage
         return redirect('index')
-    request.session['booking'] = False
+    request.session['booking'] = False   #cancel booking
     return redirect('rooms')
 
-def getUserBookings(user):
+def getUserBookings(user): #helper function to find number of user bookings
     bookings=Booking.objects.filter(user__username=user.username)
     return bookings.count()
 
 def myBookings(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:   #if user is not logged in then redirect to homepage
         return redirect('index')
     user=request.user
     bookings = Booking.objects.filter(user__username=user.username)
     images=Image.objects.all()
-    return render(request,'mybookings.html',{'bookings':bookings,'images':images})
+    return render(request,'mybookings.html',{'bookings':bookings,'images':images}) #return response with all of user bookings
